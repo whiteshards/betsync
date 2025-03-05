3d41575af57738dfbf6c4166b7a10075a6564536
Vxiws <krizz1vxiw+github@gmail.com>
2025-03-05 16:51:44 +0000

    small fixes

--- Cogs/games/cases.py
+++ Cogs/games/cases.py
@@ -27,16 +27,8 @@ class CasesPlayAgainView(discord.ui.View):
         for child in self.children:
             child.disabled = True
 
-    @discord.ui.button(label="Play Again", style=discord.ButtonStyle.success)
-    async def on_timeout(self):
-        """Handle timeout by disabling all buttons"""
-        self.disable_all_buttons()
-        if self.message:
-            try:
-                await self.message.edit(view=self)
-            except:
-                pass
-                
+    
+    @discord.ui.button(label="Play Again", style=discord.ButtonStyle.primary)           
     async def play_again(self, button, interaction):
         """Handle the play again button click"""
         if interaction.user.id != self.author_id:
@@ -75,7 +67,7 @@ class CasesPlayAgainView(discord.ui.View):
         await self.message.edit(view=self)
 
         # Run the command again
-        await cases_cog.cases(ctx, str(self.bet_amount), self.currency_used)
+        await cases_cog.cases(self.ctx, str(self.bet_amount), self.currency_used)
 
 
 class CasesCog(commands.Cog):
