b949c7eaf730c64cfe9a8ad63cedfa852210d7d3
Vxiws <krizz1vxiw+github@gmail.com>
2025-03-15 12:46:39 +0000

    Restored to 'cf40b3fd77a32524b7a2af2b0dd8358b1a580afd'
    Replit-Restored-To: cf40b3fd77a32524b7a2af2b0dd8358b1a580afd

--- Cogs/games/rockpaperscissors.py
+++ Cogs/games/rockpaperscissors.py
@@ -793,7 +793,7 @@ class RockPaperScissorsCog(commands.Cog):
                 "user_id": player_id,
                 "user_name": player.name,
                 "bet": total_bet,
-                "timestamp": int(time())
+                "timestamp": int(time.time())
             }
             server_db.update_history(channel.guild.id, server_history)
 
