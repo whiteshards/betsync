af8c78eb52b32b0e22f602a87fadeeb00951139b
siggacutie <eshoradecomair@gmail.com>
2025-07-26 13:09:05 +0000

    Restored to '7141f74ee5bff7b731afb3cfc027e5c60bbf5696'
    Replit-Restored-To: 7141f74ee5bff7b731afb3cfc027e5c60bbf5696

--- Cogs/games/pump.py
+++ Cogs/games/pump.py
@@ -251,9 +251,12 @@ class PumpGameView(discord.ui.View):
             # Force balloon to pop
             balloon_popped = True
 
-            # Consume curse and send webhook notification
-            curse_cog.consume_curse(self.ctx.author.id)
+            # Consume curse
+            #forced_loss, curse_complete = curse_cog.force_loss(self.ctx.author.id) #curse_cog.force_loss(self.ctx.author.id)
+
+            # Send webhook notification
             await self.send_curse_webhook(self.ctx.author, "pump", self.bet_amount, self.current_multiplier)
+            forced_loss, curse_complete = curse_cog.force_loss(self.ctx.author.id)
         else:
             # Calculate pop chance based on difficulty and pumps
             self.difficulty_rates = {
